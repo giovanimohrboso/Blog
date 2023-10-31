c0971970db382ac364972ade453149e87ed70319
giovanimohrboso <giovani.mohr@gmail.com>
2023-10-31 12:14:29 -0300

    deletado ideias

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -1,13 +1,19 @@
 export const datafake = [
   {"id":"1",
   "title":"Banco Fácil",
-  "description":"Projeto de Empréstimo utilizando Angular e SpringBoot",
+  "description":"Projeto de Empréstimo (Angular e SpringBoot)",
   "photoCover":"https://www.turisdogando.com.br/imagens/money-fundoazul.png",
   "link":"#"
 },
 {"id":"2",
+  "title":"Giovani Projetos",
+  "description":"Portal para Armazenagem de Projetos (Angular)",
+  "photoCover":"./assets/martelo.jpg",
+  "link":"https://giovanimohrbosoblog.netlify.app/"
+},
+{"id":"3",
   "title":"Turisdogando",
-  "description":"Portal de Viagens com Pet´s HTML5 E CSS3",
+  "description":"Portal de Viagens com Pet´s (HTML5 E CSS3)",
   "photoCover":"https://www.turisdogando.com.br/imagens/Turisdogando-fundoverde.png",
   "link":"https://www.turisdogando.com.br"
 },
