e06f56d6d2035249c528cff48e0be4a19c3e9602
giovanimohrboso <giovani.mohr@gmail.com>
2023-10-31 10:42:10 -0300

    datafake

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -5,11 +5,6 @@ export const datafake = [
   "photoCover":"https://pbs.twimg.com/media/F9c7D_vX0AAkl7C?format=jpg&name=large"
 },
 {"id":"2",
-  "title":"Blog Pessoal",
-  "description":"Blog dos Projetos feito em Angular",
-  "photoCover":"https://github.com/giovanimohrboso/Blog/blob/main/src/assets/martelo.jpg"
-},
-{"id":"3",
   "title":"Turisdogando",
   "description":"Portal de Viagens com Pet´s HTML5 E CSS3",
   "photoCover":"https://www.turisdogando.com.br/imagens/TURISDOGANDO.png"
