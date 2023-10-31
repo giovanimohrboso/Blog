291745dffdca49a9269749532f561858f7718f3e
giovanimohrboso <giovani.mohr@gmail.com>
2023-10-31 10:44:32 -0300

    Update datafake.ts

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -2,7 +2,7 @@ export const datafake = [
   {"id":"1",
   "title":"Banco Fácil",
   "description":"Projeto de Empréstimo utilizando Angular e SpringBoot",
-  "photoCover":"https://pbs.twimg.com/media/F9c7D_vX0AAkl7C?format=jpg&name=large"
+  "photoCover":"https://www.turisdogando.com.br/imagens/money.png"
 },
 {"id":"2",
   "title":"Turisdogando",
